3b35b52571143911076130d6119ec14801594d2f
zoezhao7 <zoezhao7@gmail.com>
2018-08-12 11:31:39 +0000

    Author can delete topic

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -43,6 +43,7 @@
                         {!! $topic->body !!}
                     </div>
 
+                    @can('update', $topic)
                     <div class="operate">
                         <hr>
                         <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-default btn-xs" role="button">
@@ -52,6 +53,7 @@
                             <i class="glyphicon glyphicon-trash"></i> 删除
                         </a>
                     </div>
+                    @endcan
 
                 </div>
             </div>
