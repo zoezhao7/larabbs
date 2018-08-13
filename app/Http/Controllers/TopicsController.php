eabb58d76ec0ec417a30f8b8adc694c05fe9211b
vagrant <vagrant@homestead>
2018-08-13 10:14:50 +0000

    User can delete reply

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -66,7 +66,7 @@ class TopicsController extends Controller
         $this->authorize('destroy', $topic);
         $topic->delete();
 
-        return redirect()->route('topics.index')->with('success', '删除成功！s');
+        return redirect()->route('topics.index')->with('success', '删除成功！');
     }
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
