5880686dac63e2421707f93e8835160b1c45ef46
vagrant <vagrant@homestead>
2018-08-21 07:00:27 +0000

    VerificationCode store

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\VerificationCodeRequest;
+use App\Providers\EasySmsServiceProvider;
+use Composer\Cache;
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+use Overtrue\EasySms\Exceptions\NoGatewayAvailableException;
+
+class VerificationCodesController extends Controller
+{
+    public function store(VerificationCodeRequest $request, EasySms $easySms)
+    {
+        $phone = $request->phone;
+
+        if(!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            // $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+            $code = random_int(1111, 9999);
+
+            try {
+                $result = $easySms->send($phone, [
+                    'content' => "【赵庆昌】您的验证码是{$code}。如非本人操作，请忽略本短信",
+                ]);
+            } catch (NoGatewayAvailableException $e) {
+                $message = $e->getException('yunpian')->getMessage();
+                return $this->response->errorInternal($message ?? '短信发送异常');
+            }
+        }
+        
+        $key = 'verificationCode_' . str_random(15);
+        $expiredAt = now()->addMinute(10);
+
+        \Cache::put($key, ['phone' => $phone, 'code'=>$code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expiredAt' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,15 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+    $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
 });
