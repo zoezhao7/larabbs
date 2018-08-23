0975d918e6708bf2c98e72b2177c64e6ec83517f
vagrant <vagrant@homestead>
2018-08-23 08:54:47 +0000

    locale

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => ['serializer:array', 'bindings']
+    'middleware' => ['serializer:array', 'bindings', 'change-locale']
 ], function($api) {
 
     // 用户注册相接口
